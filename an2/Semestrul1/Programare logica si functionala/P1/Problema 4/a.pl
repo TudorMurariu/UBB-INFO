31d0bd41107ffe320a1185939882aa08faf4a707
Tudor <tudorcristianmurariu@yahoo.com>
2022-10-24 14:21:49 +0300

    Am adaugat teste pentru problemele din P1

--- an2/Semestrul1/Programare logica si functionala/P1/Problema 4/a.pl
+++ an2/Semestrul1/Programare logica si functionala/P1/Problema 4/a.pl
@@ -3,11 +3,24 @@ substituire(l1..ln,e1,e2){ [], n = 0 }
                          { [l1 + substituire(l2..ln,e1,e2)], e1 != l1}
                          { [e2 + substituire(l2..ln,e1,e2)] , e1 = l1 }
 
+
+
 Predicat: substituire(L, E1, E2, R)
 L - o lista de numere intregi
 E1 - valoarea elementelor care trebuie substituite
 E2 - valoare cu care vom substitui
 R - lista raspuns formata din lista initiala cu elementele egale cu E1 sunstituite cu E2
+Modele de flux: (i, i, i, o) , (i, i, i, i) 
+
+Teste:
+substituire([],3,4,R).
+R = []
+substituire([1, 2, 1, 2, 1, 3],1,99,R).
+R = [99, 2, 99, 2, 99, 3]
+substituire([5, 5, 5],5,6,R).
+R = [6, 6, 6]
+substituire([5, 5, 5],1,6,R).
+R = [5, 5, 5]
 */
 
 
--- an2/Semestrul1/Programare logica si functionala/P1/Problema 4/b.pl
+++ an2/Semestrul1/Programare logica si functionala/P1/Problema 4/b.pl
@@ -3,10 +3,24 @@
                       { [l1 + elimina(l2..ln,X-1)], poz != 1 }
                       { l2..ln, poz == 1}
 
+
+
 Predicat: elimina(L, N, R)
 L - o lista de numere intregi
 N - un numar care reprezinta pozitia elementului pe care il vom sterge
 R - lista raspuns formata din lista L fara elementul de pe pozitia N
+Modele de flux: (i, i, o) , (i, i, i) 
+
+
+Teste:
+elimina([], 4, R).
+R = []
+elimina([1, 2, 3, 4],1,R).
+R = [2, 3, 4]
+elimina([1, 2, 3, 4],4,R).
+R = [1, 2, 3]
+elimina([1, 2, 3, 4, 5, 6],4,R).
+R = [1, 2, 3, 5, 6]
 */
 
 elimina([], _, R):- R = [].
