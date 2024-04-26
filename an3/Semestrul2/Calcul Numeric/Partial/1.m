094605fd587d5c8f126cde7077658aa9ea668c09
Tudor <tudorcristianmurariu@yahoo.com>
2024-04-26 11:31:26 +0300

    partial

---
+++ an3/Semestrul2/Calcul Numeric/Partial/1.m
@@ -0,0 +1,20 @@
+%1. Enunt: Analizati complexitatea metodei lui Gauss pentru n=4
+
+% pașii eliminarii gaussiene:
+
+% 1.Formarea matricei extinse: Se construiește o matrice
+% extinsă formată din coeficienții ecuațiilor și
+%termenii liberi ai sistemului de ecuații.
+% 2.Etapa de eliminare: Se aplică operații elementare
+% asupra rândurilor matricei extinse pentru a obține
+% o matrice superior triunghiulară.
+% 3.Etapa de substituție înapoi: Se rezolvă sistemul
+% de ecuații obținut, pornind de la ultima ecuație și
+% lucrând spre prima, folosind metoda substituției înapoi.
+
+% Pentru o matrice de dimensiune n x n,
+% complexitatea metodei lui Gauss este O(n^3)
+
+% Cu toate acestea, în practică, pentru un
+% n mic, cum ar fi n=4, complexitatea este redusă
+% și mai multe operații pot fi realizate manual
